5a6d3b9f3b8a9a5ba685c2b37419f260c82d8f39
emilielydr <emilielydr@gmail.com>
2024-11-22 09:46:29 -0500

    removedLoginSignupButton
    changed LoginSignupButton to edit profile

--- app/src/main/java/com/example/capstone/SettingsActivity.java
+++ app/src/main/java/com/example/capstone/SettingsActivity.java
@@ -13,18 +13,8 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         // Initialize button after setContentView()
-        Button btnLoginSignup = findViewById(R.id.btnLoginSignup);
         Button btnAccessibility= findViewById(R.id.btnAccessibility);
 
-        // Set the click listener for the Login button
-        btnLoginSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an Intent to open SettingsActivity
-                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
 
         btnAccessibility.setOnClickListener(new View.OnClickListener() {
             @Override
