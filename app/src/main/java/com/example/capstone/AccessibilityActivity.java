55532e80e48f669e5a323c909e285d2b2762b571
emilielydr <emilielydr@gmail.com>
2025-03-30 09:51:12 -0400

    brightnessFunctionalityDone

--- app/src/main/java/com/example/capstone/AccessibilityActivity.java
+++ app/src/main/java/com/example/capstone/AccessibilityActivity.java
@@ -1,13 +1,24 @@
 package com.example.capstone;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class AccessibilityActivity extends AppCompatActivity {
+    private SeekBar brightnessSlider;
+    private int brightness;
+    private ContentResolver contentResolver;
+    private Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,46 @@ public class AccessibilityActivity extends AppCompatActivity {
         });
         ImageButton btnBrightness = findViewById(R.id.btnBrightness);
         SeekBar brightnessSlider = findViewById(R.id.brightnessSlider);
+        contentResolver=getContentResolver();
+        window=getWindow();
+        brightnessSlider.setMax(255);
+        brightnessSlider.setKeyProgressIncrement(1);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(Settings.System.canWrite((this))){
+                Toast.makeText(this,"you can change the brightness", Toast.LENGTH_SHORT).show();
+            }else{
+                Intent intent=new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse(("package: "+getApplication().getPackageName())));
+                startActivity(intent);
+            }
+        }
+        try {
+            brightness= Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+            brightnessSlider.setProgress(brightness);
+        } catch (Settings.SettingNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        brightnessSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar brightnessSlider, int progress, boolean fromUser){
+                brightness=progress;
+                Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+                WindowManager.LayoutParams layoutParams=window.getAttributes();
+                layoutParams.screenBrightness=brightness/(float)300;
+                window.setAttributes(layoutParams);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar brightnessSlider){
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar brightnessSlider){
+
+            }
+
+        });
+
 
         btnBrightness.setOnClickListener(new View.OnClickListener() {
             @Override
