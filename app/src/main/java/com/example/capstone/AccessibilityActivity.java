f61d3d520e492c47228b16ccf35b0a933d4db1bd
emilielydr <emilielydr@gmail.com>
2025-03-30 08:15:02 -0400

    brightness slider

--- app/src/main/java/com/example/capstone/AccessibilityActivity.java
+++ app/src/main/java/com/example/capstone/AccessibilityActivity.java
@@ -3,6 +3,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class AccessibilityActivity extends AppCompatActivity {
@@ -19,5 +21,20 @@ public class AccessibilityActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ImageButton btnBrightness = findViewById(R.id.btnBrightness);
+        SeekBar brightnessSlider = findViewById(R.id.brightnessSlider);
+
+        btnBrightness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle visibility of the brightness slider
+                if (brightnessSlider.getVisibility() == View.GONE) {
+                    brightnessSlider.setVisibility(View.VISIBLE);
+                } else {
+                    brightnessSlider.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
 }
